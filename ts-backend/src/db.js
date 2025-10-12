afd259a805f6e0e734c111e869a1d024eb5c7dd3
anirudhsridhar225 <anirudhsridhar1064@gmail.com>
2025-10-13 01:42:47 +0530

    feat: code cleanup, removal of js files and changing from nodenext to commonjs

--- ts-backend/src/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-var pg_1 = require("pg");
-var node_postgres_1 = require("drizzle-orm/node-postgres");
-var schema = require("./schema.js");
-var pool = new pg_1.Pool({ connectionString: process.env.DATABASE_URL });
-exports.db = (0, node_postgres_1.drizzle)({ client: pool, schema: schema });
--- ts-backend/src/index.ts
+++ ts-backend/src/index.ts
@@ -3,9 +3,9 @@ import cors from 'cors'
 import 'dotenv/config'
 import { createExpressMiddleware } from '@trpc/server/adapters/express'
 import { toNodeHandler } from 'better-auth/node'
-import { createContext } from './trpc.js'
-import { appRouter } from './routers/index.js'
-import { auth } from './auth.js'
+import { createContext } from './trpc'
+import { appRouter } from './routers/index'
+import { auth } from './auth'
 import morgan from 'morgan'
 
 const app = express()
